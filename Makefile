17cdbd1bbfe28bb8e6904dec2737f29e724e7324
Jonathan Behrens <fintelia@gmail.com>
2018-11-20 16:19:44 -0500

    Fix entry name

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all: src/*.rs Cargo.toml
-	xargo rustc --target riscv32imac-unknown-none-elf -- -C link-arg=-Tsrc/linker.ld -C link-arg=--gc-keep-exported -C linker=riscv32-unknown-elf-ld -Z linker-flavor=ld
+	xargo rustc --target riscv32imac-unknown-none-elf -- -C link-arg=-Tsrc/linker.ld  -C linker=riscv32-unknown-elf-ld -Z linker-flavor=ld
 
 svpn: *.c Makefile
 	riscv64-unknown-elf-gcc -O0 -nostdlib *.c -o svpn
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,11 @@
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
 #[panic_handler] fn panic(_info: &::core::panic::PanicInfo) -> ! {loop {}}
-#[no_mangle] fn abort(){ loop{} }
-
-#[start]
-fn start(argc: isize, argv: *const *const u8) -> isize { 0 }
+#[start] fn start(_argc: isize, _argv: *const *const u8) -> isize { 0 }
 
 #[link_section = ".text.init"]
 #[no_mangle]
-fn entry() {
+fn _start() {
     let mut i = 21;
     loop {
         if i % 2 == 0 {
