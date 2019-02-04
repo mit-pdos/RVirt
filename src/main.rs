8e73a743626ba84c17dcf1ce4f91ce57c77fd3f2
Jonathan Behrens <fintelia@gmail.com>
2019-02-04 11:17:16 -0500

    Initialize registers before entering guest OS

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,38 @@ fn _start2(_hartid: usize, device_tree_blob: usize) {
         }
 
 //        csrs!(mstatus, 1 << 11);
-        asm!("mret" :::: "volatile");
+        asm!("mv a1, $0
+              li ra, 0
+              li sp, 0
+              li gp, 0
+              li tp, 0
+              li t0, 0
+              li t1, 0
+              li t2, 0
+              li s0, 0
+              li s1, 0
+              li a0, 0
+              li a2, 0
+              li a3, 0
+              li a4, 0
+              li a5, 0
+              li a6, 0
+              li a7, 0
+              li s2, 0
+              li s3, 0
+              li s4, 0
+              li s5, 0
+              li s6, 0
+              li s7, 0
+              li s8, 0
+              li s9, 0
+              li s10, 0
+              li s11, 0
+              li t3, 0
+              li t4, 0
+              li t5, 0
+              li t6, 0
+              mret" :: "r"(device_tree_blob) :: "volatile");
     }
     unreachable!();
 }
