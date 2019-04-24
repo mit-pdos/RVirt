300d7fe1689d4f10d59c60c451414521df831de4
Cel A. Skeggs <cela@mit.edu>
2019-04-24 00:39:01 -0400

    trap: add strings to explain exception causes

--- src/trap.rs
+++ src/trap.rs
@@ -57,6 +57,35 @@ pub mod constants {
     pub const SCAUSE_INSN_PAGE_FAULT: u64 = 12;
     pub const SCAUSE_LOAD_PAGE_FAULT: u64 = 13;
     pub const SCAUSE_STORE_PAGE_FAULT: u64 = 15;
+
+    pub const CAUSE_STRINGS: [&str; 16] = [
+        "Instruction address misaligned",
+        "Instruction access fault",
+        "Illegal instruction",
+        "Breakpoint",
+        "Load address misaligned",
+        "Load access fault",
+        "Store/AMO address misaligned",
+        "Store/AMO access fault",
+        "Environment call from U-mode",
+        "Environment call from S-mode",
+        "Reserved (10)",
+        "Environment call from M-mode",
+        "Instruction page fault",
+        "Load page fault",
+        "Reserved (13)",
+        "Store/AMO page fault"
+    ];
+
+    pub fn cause_to_str(cause: u64) -> &'static str {
+        if (cause as i64) < 0 {
+            "Interrupt"
+        } else if cause >= 16 {
+            "Reserved (>=16)"
+        } else {
+            CAUSE_STRINGS[cause as usize]
+        }
+    }
 }
 use self::constants::*;
 
